87d971002a0c02569104cbe12073e683514fc0e6
Joaquín Magneres <joacomag@gmail.com>
2011-06-23 03:46:22 +0000

    Vuelta para atras con el plugin de drools

--- tp-fallas-grails/grails-app/conf/BuildConfig.groovy
+++ tp-fallas-grails/grails-app/conf/BuildConfig.groovy
@@ -7,7 +7,6 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
-        excludes "xml-apis"
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
